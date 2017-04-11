def8ee5ebc174f5d0fb560d6d2013639d8ea4b0b
Aluno <aluno@band.net>
2017-04-10 21:49:10 -0300

    Edicionado Exercicio 17 b do dia 10/04/2017

---
+++ aula18/Exercicio17b.java
@@ -0,0 +1,28 @@
+package aula18;
+
+import java.util.Scanner;
+
+public class Exercicio17b {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Quantidade de produto comprados: ");
+        int produtos = s.nextInt();
+        s.nextLine();
+        double[] precos = new double[produtos];
+        for (int i = 0; i < produtos; i++) {
+            int produtoAtual = i + 1;
+            System.out.print("Digite o valor do produto " + produtoAtual + ": ");
+            double preco = s.nextDouble();
+            s.nextLine();
+            System.out.print("Digite a quantidade de produtos comprados: ");
+            int quantidade = s.nextInt();
+            s.nextLine();
+            precos[i] = preco * quantidade;
+        }
+        
+        for (int i = 0; i < precos.length; i++) {
+            System.out.println("Valor total da compra do produto " + (i + 1) + ": " + precos[i]);
+        }
+    }
+}
