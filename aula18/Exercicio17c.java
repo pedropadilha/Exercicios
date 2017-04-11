9a2decd89c8673126c43dd31806905629fcc01a1
Pedro Padilha <pedropadilha@me.com>
2017-04-10 22:45:12 -0300

    Adicionado Exercício 17c do dia 10/04/2017

---
+++ aula18/Exercicio17c.java
@@ -0,0 +1,30 @@
+package aula18;
+
+import java.util.Scanner;
+
+public class Exercicio17c {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Quantidade de boletins que vão ser cadastrados: ");
+        int totalBoletins = s.nextInt();
+        s.nextLine();
+        String[] boletins = new String[totalBoletins];
+        
+        for(int i = 0; i < boletins.length; i++) {
+            System.out.print("Digite o nome do aluno" + (i + 1) + ": ");
+            String nome = s.nextLine();
+            System.out.print("Digite a nota 1 do aluno " + nome + ": ");
+            double nota1 = s.nextDouble();
+            s.nextLine();
+            System.out.print("Digite a nota 2 do aluno " + nome + ": ");
+            double nota2 = s.nextDouble();
+            s.nextLine();
+            double media = ((nota1 + nota2) / 2);
+            boletins[i] = (i + 1) + ". " + nome + ". Média " + media + ". " + (media >= 5 ? "A" : "RE") + "PROVADO";
+        }
+        for (String boletim : boletins) {
+            System.out.println(boletim);
+        }
+        
+    }
+}
